a731b60d088791073399f94d614db2c0f169a871
Heorhii Siburov <g.siburov@gmail.com>
2019-10-09 14:17:33 -0700

    GET requests working

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,9 +1,22 @@
 const db = require('../data/db-config.js');
 
 module.exports = {
-    find
+    find,
+    findById,
+    findSteps
 }
 
 function find() {
     return db('schemes');
+}
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+}
+
+function findSteps(stepID) {
+    return db('steps')
+    .join('schemes', 'schemes.id', '=', 'steps.scheme_id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+    .where({ scheme_id: stepID });
 }
